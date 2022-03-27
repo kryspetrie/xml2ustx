56a8360a5df7ad6866b256fcaf04320edd390e0b
nicolalandro <nicolaxx94@Live.it>
2022-03-27 14:30:44 +0200

    read rest and offset

--- src/main.py
+++ src/main.py
@@ -8,6 +8,14 @@ file_path = './data/haendel_hallelujah.xml'
 music = music21.converter.parse(file_path)
 
 for part in music.parts:
-    print('Part:', part._partName)
-    for n in part.flat.notes:
-        print(f"\tNote: {n.pitch.name}{n.pitch.octave} {n.duration.quarterLength:0.01f} - {n.lyric}")
+    print('Part:', part.partName)
+
+    for event in part.flat:
+        for y in event.contextSites():
+            if y[0] is part:
+                offset=y[1]
+        if getattr(event,'isNote',None) and event.isNote:
+            print(f"\tNote: {event.pitch.name}{event.pitch.octave}, Duration: {event.quarterLength}, Midi pitch: {event.pitch.midi}, Offset: {offset}")
+        if getattr(event,'isRest',None) and event.isRest:
+            print(f"\tRest, Duration: {event.quarterLength}, Offset: {offset}")
+    break
