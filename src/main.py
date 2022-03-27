d1f13ca763492bd57eb00f3c83b04623867d83a7
nicolalandro <nicolaxx94@Live.it>
2022-03-27 14:13:58 +0200

    multiple part read

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ import music21
 
 
 # file_path = './data/en_test.xml'
-file_path = './data/jappo_test.xml'
+# file_path = './data/jappo_test.xml'
+file_path = './data/haendel_hallelujah.xml'
 
 music = music21.converter.parse(file_path)
 
-for n in music.flat.notes:
-    print(f"Note: {n.pitch.name}{n.pitch.octave} {n.duration.quarterLength:0.01f} - {n.lyric}")
+for part in music.parts:
+    print('Part:', part._partName)
+    for n in part.flat.notes:
+        print(f"\tNote: {n.pitch.name}{n.pitch.octave} {n.duration.quarterLength:0.01f} - {n.lyric}")
